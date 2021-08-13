9e551c9f2b98885aecf59ef51aff2ec0dbbb8044
unknown <vinecx>
2021-08-13 15:00:28 -0300

    Teste

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const path = require('path');
 const app = express(),
       bodyParser = require("body-parser");
       port = process.env.PORT || 3000;
 
-    
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../my-app/build')));
 require('./Routes/index')(app);
